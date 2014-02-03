d22034ee7e8c36dc6d054cc241e1c7ac266794bc
Nick Veys <nick@codelever.com>
2014-02-03 14:47:30 -0600

    Adding remaining read-only commands

--- lib/cgminer/api/client.rb
+++ lib/cgminer/api/client.rb
@@ -8,16 +8,65 @@ module CGMiner
         @port = port
       end
 
+      def asc(index)
+        command(:asc, index)
+      end
+
+      def asccount
+        command(:asccount)
+      end
+
+      # @param command [Symbol] the command to check
+      def check(command)
+        command(:check, command)
+      end
+
+      def coin
+        command(:coin)
+      end
+
+      def config
+        command(:config)
+      end
+
+      def devdetails
+        command(:devdetails)
+      end
+
       def devs
         command(:devs)
+      end
+
+      def gpu(index)
+        command(:gpu, index)
+      end
+
+      def gpucount
+        command(:gpucount)
+      end
+
+      def pga(index)
+        command(:pga, index)
+      end
+
+      def pgacount
+        command(:pgacount)
       end
 
       def pools
         command(:pools)
       end
 
+      def stats
+        command(:stats)
+      end
+
       def summary
         command(:summary)
+      end
+
+      def usbstats
+        command(:usbstats)
       end
 
       def version
@@ -26,9 +75,9 @@ module CGMiner
 
       private
 
-      def command(symbol)
+      def command(symbol, *parameters)
         api = Net::Telnet::new('Host' => @host, 'Port' => @port)
-        results = api.cmd({ command: symbol.to_s }.to_json)
+        results = api.cmd({ command: symbol.to_s, parameter: parameters.join(',') }.to_json)
 
         begin
           json = JSON.parse(results)
